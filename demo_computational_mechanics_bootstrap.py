2b4deffcb0d675c13f04e9fdc2629c6c9b86c065
David Darmon <gendarmon@gmail.com>
2020-07-31 10:34:05 -0400

    Adding Jupyter Notebook with demo of computational mechanics bootstrap.

--- demo_computational_mechanics_bootstrap.py
+++ demo_computational_mechanics_bootstrap.py
@@ -1,32 +1,35 @@
-import itertools
-
-import numpy
-
 from transCSSR_bc import *
 
-import matplotlib.pyplot as plt
+# Simulate a new time series:
 
-save_prefix = ''
-
-# Length of original time series
+# Length of simulated time series
 N = 1000
 
-# Number of bootstrap time series
-B = 200
+Yt_name = 'even'
 
-# Yt_name = 'even'
-Yt_name = 'complex-csm'
-
-axs = ['0']
 ays = ['0', '1']
-
-e_symbols = list(itertools.product(axs, ays)) # All of the possible pairs of emission
-                                              # symbols for (x, y)
 
 transducer_fname_true = 'transCSSR_results/+{}.dot'.format(Yt_name)
 
 stringY = simulate_eM_fast(N, transducer_fname_true, ays, 'transCSSR')
 
-alpha = 0.001
+# Perform computational mechanics bootstrap:
 
-boot_out = computational_mechanics_bootstrap(stringY, ays, Yt_name_inf = '{}_inf'.format(Yt_name), B = B, alpha = 0.001)
+B = 2000 # Number of bootstrap time series to generate.
+
+boot_out = computational_mechanics_bootstrap(stringY, ays, Yt_name_inf = '{}_inf'.format(Yt_name), B = B)
+
+# Compute true measures for original eM:
+
+HLs, hLs, hmu, ELs, E, Cmu, etas_matrix = compute_ict_measures(transducer_fname_true, ays, inf_alg = 'transCSSR', L_max = 10, to_plot = False)
+
+measures_true = {'Cmu' : Cmu, 'hmu' : hmu, 'E' : E}
+
+# Check coverage of confidence intervals:
+
+conf_level = 0.95
+alpha = 1 - conf_level
+
+for measure_name in measures_true.keys():
+	ci = boot_out['Q'][measure_name]([alpha/2, 1-alpha/2])
+	print('{} : {}% CI [{}, {}], true = {}'.format(measure_name, conf_level*100, ci[0], ci[1], measures_true[measure_name]))
